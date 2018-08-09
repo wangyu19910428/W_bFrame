f65b2d359bfc9eed621481a2dfd218f2893008d2
wangyu <13633167649@163.com>
2018-08-09 16:43:32 +0800

    fix webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,18 @@ const SRC_PATH = utils.fullPath('src');
 const DIST_PATH = utils.fullPath('dist');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-//判断是否为测试环境
-const __DEV__ = process.env.NODE_ENV !== 'production';
 
 let options = {
     devtool: 'inline-source-map',
-    mode: __DEV__ ? 'development' : 'production',
+    mode: 'development',
     entry: {
         app: SRC_PATH + '/router.js'
     },
     output: {
         filename: 'js/[name].js',
-        path: DIST_PATH
+        path: DIST_PATH,
+        publickPath: '/',
+        chunkFilename: 'js/[name].js'
     },
     module: {
         rules: [
@@ -35,12 +35,9 @@ let options = {
         new HtmlWebpackPlugin({
             filename: utils.fullPath('./') + '/index.html',
             template: SRC_PATH + '/tpl.html'
-        })
+        }),
+        new CleanWebpackPlugin(['dist/*'])
     ]
 }
-
-__DEV__&& options.plugins.push(
-    new CleanWebpackPlugin(['dist/*'])
-)
 
 module.exports = options;
