c0c4cf126ec4bdf3ae1345bff2957d0a5799989f
wangyu <13633167649@163.com>
2019-01-24 17:29:54 +0800

    react 15 dialog

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,10 +1,14 @@
 import './index.scss';
 import { Button, DatePicker, Input, Table, Card, Select } from 'antd';
+import Dialog from '../../component/Dialog/index';
 const Options = Select.Option;
 
 export default class Home extends Component{
     constructor(props) {
         super(props);
+        this.state = {
+            visible: false
+        };
     }
 
     componentWillMount = () => {
@@ -13,8 +17,17 @@ export default class Home extends Component{
     
 
     handleCLick = () => {
-        this.props.history.push({pathname: '/', query: {id: 1}, search: '?id=1'});
-        
+        // this.props.history.push({pathname: '/', query: {id: 1}, search: '?id=1'});
+        this.setState({
+            visible: true
+        });
+    }
+
+    dialogClick = () => {
+        this.setState({
+            visible: false
+        });
+        alert()
     }
 
     render () {
@@ -25,6 +38,15 @@ export default class Home extends Component{
                 </svg>
                 Home
                 <Button type='primary' onClick={this.handleCLick} >gaibna</Button>
+                {
+                    this.state.visible? 
+                    <div style={{height: 300, background: 'red'}} onClick={this.dialogClick}>
+                        <Dialog  >
+                            <div >1234567890</div>
+                        </Dialog>
+                    </div>
+                    : null
+                }
             </div>
         )
     }
