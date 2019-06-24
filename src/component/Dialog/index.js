01f48cfd5a46fe4c1de552c432b63b04e4168a3d
wangyu <13633167649@163.com>
2019-06-24 17:33:49 +0800

    merge

--- src/component/Dialog/index.js
+++ src/component/Dialog/index.js
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-import { unstable_renderSubtreeIntoContainer, unmountComponentAtNode } from 'react-dom';
-import './index.scss';
-
-export default class Dialog extends Component{
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-
-        }
-    }
-    render () {
-        return null;
-    }
-    
-    componentDidMount () {
-        const doc = window.document;
-        this.node = doc.createElement('div');
-        doc.body.appendChild(this.node);
-
-        this.renderPortal(this.props);
-    }
-
-    componnetDIdUpdate() {
-        this.renderPortal(this.props);
-    }
-
-    componentWillUnmount() {
-        unmountComponentAtNode(this.node);
-        window.document.body.removeChild(this.node);
-    }
-
-    renderPortal (props) {
-        unstable_renderSubtreeIntoContainer(
-            this, //代表当前组件
-            <div className='dialog'> 
-                {props.children}
-            </div>, //塞进传送门的JSX
-            this.node //传送门另一端的DOM node
-        )
-    }
-=======
 import {createPortal} from 'react-dom';
 import './index.scss';
 
@@ -65,5 +22,4 @@ export default class Dialog extends Component{
     componentWillUnmount () {
         window.document.body.removeChild(this.node);
     }
->>>>>>> master
 }
